d9c9d86a5607751a241666f60304cc23e5964035
chen-charles <chen-charles@users.noreply.github.com>
2016-10-06 23:53:55 -0400

    Initial

---
+++ type.h
@@ -0,0 +1,27 @@
+#ifndef _LIB_TYPE_H_
+#define _LIB_TYPE_H_
+
+//Conventions
+#include	<stdint.h>
+#include	<stddef.h>
+#include	<stdbool.h>
+
+typedef unsigned char byte;
+typedef uintptr_t uintreg_t;	//general purpose register uint repr.
+
+#ifdef __cplusplus
+#define EXTERN_C extern "C" {
+#define EXTERN_C_END } 
+#else
+#define EXTERN_C 
+#define EXTERN_C_END 
+#endif	//__cplusplus
+
+typedef struct
+{
+	uintptr_t ptr;
+	size_t len;
+}
+memblock;
+
+#endif
